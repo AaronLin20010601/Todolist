ebfe415259c2190ecd5e990b3b127748a1204341
aaronlin <aaronlin@ukn.edu.tw>
2025-04-22 14:14:55 +0800

    remove uneccessary part of frontend api code

--- Todolist_Frontend/src/api/reset.js
+++ Todolist_Frontend/src/api/reset.js
@@ -2,17 +2,6 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:5000/api/reset'
 
-// 發送驗證碼
-export const sendVerificationCode = async (email) => {
-    try {
-        const response = await axios.post(`${API_URL}/verification-code`, { email })
-        return response.data
-    }
-    catch (error) {
-        throw error
-    }
-}
-
 // 送出重設密碼
 export const reset = async (payload) => {
     try {
